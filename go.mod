c1a6abfd63d8369c0937c6e78e3eafb2bf98121d
eloizhang <eloizhang@tencent.com>
2020-04-21 01:25:33 +0800

    redis succed

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module eloizhang/goConsumer
 
 go 1.13
 
-require github.com/Shopify/sarama v1.26.1
+require (
+	github.com/Shopify/sarama v1.26.1
+	github.com/go-redis/redis/v7 v7.2.0
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	google.golang.org/appengine v1.6.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	kafka "eloizhang/goConsumer/kafkautils"
-	"log"
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis/v7"
 	"os/signal"
 	"runtime"
 	"syscall"
 )
 
-type message struct {
-	roomId string
-	userId string
-	time   string
-	msg    string
+type request struct {
+	RoomId  string `json:"room_id"`
+	UserId  string `json:"user_id"`
+	Time    float64    `json:"time"`
+	Message `json:"msg"`
 }
+
+type Message struct {
+	Color   string `json:"color"`
+	Content string `json:"content"`
+	Speed   string `json:"speed"`
+}
+
 
 func main() {
 	// 初始化消费者
@@ -29,5 +38,31 @@ func main() {
 }
 
 func TopicCallBack(data []byte) {
-	log.Printf("kafka Test:" + string(data))
+	//log.Printf("kafka Test:" + string(data))
+	var req request
+	err := json.Unmarshal([]byte(data), &req)
+	//fmt.Println(req)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+
+	err = client.ZAdd(req.RoomId, &redis.Z{
+		Score:  req.Time,
+		Member: data,
+	}).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	vals, err := client.ZRevRange(req.RoomId,0,-1).Result()
+	if err != nil {
+		panic(err)
+	}
+	for _, val := range vals {
+		fmt.Println(val)
+	}
 }
